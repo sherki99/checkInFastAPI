4bd003716a1a1f3c84d67aead909c89de252427e
sherki99 <cherkimeziane5@gmail.com>
2025-02-03 18:51:30 +0100

    ll

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import uvicorn 
+import os
+import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host= "0.0.0.0", port = 8000, reload=True)
+    port = int(os.getenv("PORT", 8000))  # Get port from environment variable or default to 8000
+    host = "0.0.0.0"  # This is the standard for making the app externally accessible
+    reload = os.getenv("ENV") != "production"  # Only reload in development
 
+    uvicorn.run("api:app", host=host, port=port, reload=reload)
