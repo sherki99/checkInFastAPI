677d70332738c0f3ae0057b8c23a5ae83b7c6dad
Cherki Meziane <101719089+sherki99@users.noreply.github.com>
2025-03-02 13:17:07 +0100

    ee

--- api.py
+++ api.py
@@ -253,7 +253,7 @@ from check_time_plans.plans.meal_plan_generator import MealPlanGenerator
 
 class CheckInData(BaseModel):
     userId: str
-    mealPlanLastWeek: str
+    analysisReport: Dict[str]
     analysisReportStart: str
     bodyMeasurementsLastWeek: str
     dailyReportsLastWeek: str
@@ -261,10 +261,11 @@ class CheckInData(BaseModel):
     userWorkoutDetailsLastWeek: str
 
 @app.post("/check_in_optimization/")
-async def process_check_in(data: CheckInData):
+async def process_check_in(data: BaseModel):
     try:
         # Convert input data to dictionary
         check_in_data = data.dict()
+        
 
         # Store original plans
         #original_workout_plan = check_in_data['userWorkoutDetailsLastWeek']
@@ -273,6 +274,7 @@ async def process_check_in(data: CheckInData):
         #1. Data Ingestion Phase (same as before)
         ingestion_module = CheckInDataIngestionModule()
         standardized_data = ingestion_module.process_check_in_data(check_in_data)
+        print(standardized_data[:100])
 
         meal_data = MealAdherenceExtractor().extract_meal_adherence(standardized_data.mealPlan)
         training_data = TrainingLogsExtractor().extract_training_logs(standardized_data.exerciseLogs)
